076290c6530810f3d94e7ba2a318a7a23a2ec410
Luis Boscan <luife642@gmail.com>
2016-11-18 21:24:33 -0400

    SearchBar component added

---
+++ app/components/SearchBar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export class SearchBar extends React.Component {
+	constructor(props) {
+			super(props);
+
+			this.state = { term: '' };
+			this.onInputChange = this.onInputChange.bind(this);
+	}
+
+	onInputChange(e) {
+		this.setState({term: e.target.value});
+	}
+
+	render() {
+		return (
+			<form className="input-group">
+				<input
+					placeholder="Get a five-day forecast in your favorite cities"
+					className="form-control"
+					value={this.state.term}
+					onChange={this.onInputChange}
+					/>
+    <span className="input-group-btn">
+     <button type="submit" className="btn btn-secondary">Submit</button>
+    </span>
+			</form>
+		);
+	}
+}
+
+export default SearchBar;
